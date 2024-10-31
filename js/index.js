9f316c4892a239fa5296a3de306db59881bb561e
Chrysthofer <chrysthofer.afonso@estudante.ufjf.br>
2024-10-31 17:11:40 -0300

    Commit 10

--- js/index.js
+++ js/index.js
@@ -3,3 +3,20 @@ function translateSite(language) {
     alert(`Switching site language to ${language}`);
     // Logic for changing the language (fetching translations, etc.)
 }
+
+// Function to load translations from the JSON file
+async function loadTranslations() {
+    const response = await fetch('language.json');
+    return await response.json();
+}
+
+// Function to apply translations based on the selected language
+function translatePage(translations, lang) {
+    const elementsToTranslate = document.querySelectorAll('[data-i18n]');
+    elementsToTranslate.forEach((element) => {
+        const key = element.getAttribute('data-i18n');
+        if (translations[lang] && translations[lang][key]) {
+            element.innerHTML = translations[lang][key];
+        }
+    });
+}
